fad1b130a52986085a466910acced5ec38c41579
erthalion <9erthalion6@gmail.com>
2016-11-08 14:12:16 +0700

    Update templates

--- templates/sql/mysql_init.sql
+++ templates/sql/mysql_init.sql
@@ -1,1 +1,5 @@
-create table usertable(data json);
+create table usertable (
+    data json,
+    ycsb_key char(100) generated always as (json_extract(data, '$.YCSB_KEY')),
+    index usertable_data_idx (ycsb_key)
+);
--- templates/sql/pg_init.sql
+++ templates/sql/pg_init.sql
@@ -1,1 +1,13 @@
 create table usertable(data jsonb);
+create index usertable_data_idx on usertable ((data->>'YCSB_KEY'));
+-- {% if field_index and nested %}
+-- create index usertable_data_idx on usertable ((data->>{% for i in range(9, 0, -1) %}'YCSB_KEY{{i}}'->>{% endfor %}'YCSB_KEY'));
+-- {% elif field_index %}
+-- create index usertable_data_idx on usertable ((data->>'YCSB_KEY'));
+-- {% else %}
+-- create index usertable_data_idx on usertable using gin(data jsonb_path_ops);
+-- {% endif %}
+alter table usertable set (autovacuum_vacuum_scale_factor = 0.0);
+alter table usertable set (autovacuum_vacuum_threshold = 10000);
+alter table usertable set (autovacuum_analyze_scale_factor = 0.0);
+alter table usertable set (autovacuum_analyze_threshold = 10000);
