c34b2001a67513449c6a90bc3eac9c32ed3e49f0
erthalion <9erthalion6@gmail.com>
2016-11-08 14:23:06 +0700

    Update test scripts

--- templates/sql/pg_init.sql
+++ templates/sql/pg_init.sql
@@ -1,12 +1,13 @@
 create table usertable(data jsonb);
+
+{% if field_index and nested %}
+create index usertable_data_idx on usertable ((data->>{% for i in range(9, 0, -1) %}'YCSB_KEY{{i}}'->>{% endfor %}'YCSB_KEY'));
+{% elif field_index %}
 create index usertable_data_idx on usertable ((data->>'YCSB_KEY'));
--- {% if field_index and nested %}
--- create index usertable_data_idx on usertable ((data->>{% for i in range(9, 0, -1) %}'YCSB_KEY{{i}}'->>{% endfor %}'YCSB_KEY'));
--- {% elif field_index %}
--- create index usertable_data_idx on usertable ((data->>'YCSB_KEY'));
--- {% else %}
--- create index usertable_data_idx on usertable using gin(data jsonb_path_ops);
--- {% endif %}
+{% else %}
+create index usertable_data_idx on usertable using gin(data jsonb_path_ops);
+{% endif %}
+
 alter table usertable set (autovacuum_vacuum_scale_factor = 0.0);
 alter table usertable set (autovacuum_vacuum_threshold = 10000);
 alter table usertable set (autovacuum_analyze_scale_factor = 0.0);
