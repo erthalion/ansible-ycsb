5f3a8f2a08421e85d0d5edda46829ba5fcbc28d0
erthalion <9erthalion6@gmail.com>
2017-06-14 22:38:01 +0200

    Possibility to use custom table name and create view from jsonb

--- templates/init/pg_init.sql
+++ templates/init/pg_init.sql
@@ -16,3 +16,19 @@ alter table usertable set (autovacuum_vacuum_scale_factor = 0.0);
 alter table usertable set (autovacuum_vacuum_threshold = 10000);
 alter table usertable set (autovacuum_analyze_scale_factor = 0.0);
 alter table usertable set (autovacuum_analyze_threshold = 10000);
+
+{% if sql_view is defined %}
+create type ycsb_row as (
+	YCSB_KEY VARCHAR(255) PRIMARY KEY,
+	FIELD0 TEXT, FIELD1 TEXT,
+	FIELD2 TEXT, FIELD3 TEXT,
+	FIELD4 TEXT, FIELD5 TEXT,
+	FIELD6 TEXT, FIELD7 TEXT,
+	FIELD8 TEXT, FIELD9 TEXT
+);
+
+create view usertable_view as
+    select r.*
+    from usertable,
+         jsonb_populate_record(NULL::ycsb_row, data) r;
+{% endif %}
