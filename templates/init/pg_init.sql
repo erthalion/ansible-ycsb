4e2ba983a9ae51f089e645300f8f466169bcf0dd
erthalion <9erthalion6@gmail.com>
2017-06-17 17:54:09 +0200

    Fix init typo

--- templates/init/pg_init.sql
+++ templates/init/pg_init.sql
@@ -23,8 +23,8 @@ create type ycsb_row as (
 	"FIELD2" TEXT, "FIELD3" TEXT,
 	"FIELD4" TEXT, "FIELD5" TEXT,
 	"FIELD6" TEXT, "FIELD7" TEXT,
-	"FIELD8" TEXT, "FIELD9" TEXT
-	"FIELD10" TEXT, "FIELD11" TEXT,
+	"FIELD8" TEXT, "FIELD9" TEXT,
+	"FIELD10" TEXT, "FIELD11" TEXT
 );
 
 create view usertable_view as
