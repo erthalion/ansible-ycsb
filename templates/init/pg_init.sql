e9faefb2654280332849e25d65012ec39a8d55d5
erthalion <9erthalion6@gmail.com>
2017-06-18 16:22:06 +0200

    Possibility of comparison with plain table

--- templates/init/pg_init.sql
+++ templates/init/pg_init.sql
@@ -1,7 +1,27 @@
-{% if json_type is defined %}
-create table usertable(data json);
-{% else %}
-create table usertable(data jsonb);
+{% if sql is not defined %}
+    {% if json_type is defined %}
+    create table usertable(data json);
+    {% else %}
+    create table usertable(data jsonb);
+    {% endif %}
+{% endif %}
+
+{% if sql is defined %}
+create table usertable (
+	YCSB_KEY VARCHAR(255),
+	FIELD0 TEXT, FIELD1 TEXT,
+	FIELD2 TEXT, FIELD3 TEXT,
+	FIELD4 TEXT, FIELD5 TEXT,
+	FIELD6 TEXT, FIELD7 TEXT,
+	FIELD8 TEXT, FIELD9 TEXT
+);
+
+create index usertable_ycsb_key_idx on usertable (YCSB_KEY);
+
+-- create view usertable_view as
+    -- select r.*
+    -- from usertable,
+         -- jsonb_populate_record(NULL::ycsb_row, data) r;
 {% endif %}
 
 {% if field_index is defined and nested is defined %}
@@ -16,22 +36,6 @@ alter table usertable set (autovacuum_vacuum_scale_factor = 0.0);
 alter table usertable set (autovacuum_vacuum_threshold = 10000);
 alter table usertable set (autovacuum_analyze_scale_factor = 0.0);
 alter table usertable set (autovacuum_analyze_threshold = 10000);
-
-{% if sql_view is defined %}
-create type ycsb_row as (
-	"YCSB_KEY" VARCHAR(255),
-	"FIELD2" TEXT, "FIELD3" TEXT,
-	"FIELD4" TEXT, "FIELD5" TEXT,
-	"FIELD6" TEXT, "FIELD7" TEXT,
-	"FIELD8" TEXT, "FIELD9" TEXT,
-	"FIELD10" TEXT, "FIELD11" TEXT
-);
-
-create view usertable_view as
-    select r.*
-    from usertable,
-         jsonb_populate_record(NULL::ycsb_row, data) r;
-{% endif %}
 
 {% if jsonb_check is defined %}
 alter table usertable add constraint field_constrains check (data ? 'FIELD0');
