77cb27c7d340582f2081c19a4ff139747810e466
erthalion <9erthalion6@gmail.com>
2017-10-01 13:31:47 +0200

    Improve index for mysql

--- templates/init/mysql_init.sql
+++ templates/init/mysql_init.sql
@@ -1,5 +1,4 @@
 create table usertable (
     data json,
-    ycsb_key char(100) generated always as (json_extract(data, '$.YCSB_KEY')) stored primary key,
-    index usertable_data_idx (ycsb_key)
+    ycsb_key varchar(255) generated always as (json_extract(data, '$.YCSB_KEY')) stored primary key
 );
