d5b38c8a719f1efa1b022757ebc6fd676c53e378
erthalion <9erthalion6@gmail.com>
2018-05-27 22:46:38 +0200

    Adjustmens for speed

--- templates/init/nvme_generate.sql
+++ templates/init/nvme_generate.sql
@@ -5,49 +5,51 @@ create table test_nvme(
     data jsonb
 );
 
-insert into test_nvme
-select s.id, 'name' || s.id, current_timestamp - (s.id || ' hour')::interval, ('["' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text || '", "' ||
-    md5(random()::text)::text ||
-'"]')::jsonb
-from generate_series(1, {{ recordcount }}) as s(id);
+copy test_nvme from '/home/postgres/data';
+
+-- insert into test_nvme
+-- select s.id, 'name' || s.id, current_timestamp - (s.id || ' hour')::interval, ('["' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text || '", "' ||
+    -- md5(random()::text)::text ||
+-- '"]')::jsonb
+-- from generate_series(1, {{ recordcount }}) as s(id);
 
 vacuum analyze;
--- templates/init/nvme_test.sql
+++ templates/init/nvme_test.sql
@@ -1,2 +1,48 @@
 set work_mem = 64;
-select * from test_nvme order by time limit 10000;
+
+insert into test_nvme
+select s.id, 'name' || s.id, current_timestamp - (s.id || ' hour')::interval, ('["' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text || '", "' ||
+    md5(random()::text)::text ||
+'"]')::jsonb
+from generate_series(1, 1) as s(id);
+
+select * from (select * from test_nvme limit 10000) q order by time ;
