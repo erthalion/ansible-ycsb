a75da41f6574d2bc961043943c449fa58d9e5412
erthalion <9erthalion6@gmail.com>
2018-06-15 09:28:55 +0200

    Add clock adjustment

--- run.sh
+++ run.sh
@@ -13,10 +13,33 @@ do
     do
         echo "Run ${i} experiment"
 
-        ansible-playbook benchmark.yml -e "pgbench_clients=${thread}" -e "@${1}" || failed=1
+        ansible-playbook benchmark.yml\
+            -e "pgbench_clients=${thread}"\
+            -e "INSTANCE_TYPE=m4.xlarge"\
+            -e "@${1}" || failed=1
 
         # Forced cleanup even if there were errors before
         #
+        ansible-playbook cleanup.yml -e "@${1}"
+
+        ansible-playbook benchmark.yml\
+            -e "pgbench_clients=${thread}"\
+            -e "INSTANCE_TYPE=m4.xlarge"\
+            -e "tsc=true"\
+            -e "@${1}" || failed=1
+        ansible-playbook cleanup.yml -e "@${1}"
+
+        ansible-playbook benchmark.yml\
+            -e "pgbench_clients=${thread}"\
+            -e "INSTANCE_TYPE=m5.xlarge"\
+            -e "@${1}" || failed=1
+        ansible-playbook cleanup.yml -e "@${1}"
+
+        ansible-playbook benchmark.yml\
+            -e "pgbench_clients=${thread}"\
+            -e "INSTANCE_TYPE=m5.xlarge"\
+            -e "tsc=true"\
+            -e "@${1}" || failed=1
         ansible-playbook cleanup.yml -e "@${1}"
 
         if [[ ${failed:+x} ]]; then
